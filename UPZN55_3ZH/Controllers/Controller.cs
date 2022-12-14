cb9f04383e97bc4dbde8506d4982cd9217500733
Kisking <kisfaludy.kinga@stud.uni-corvinus.hu>
2022-12-14 13:03:34 +0100

    Add project files.

---
+++ UPZN55_3ZH/Controllers/Controller.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UPZN55_3ZH.Controllers
+{
+
+    [Route("api/jokes")]
+    [ApiController]
+    public class JokeController : ControllerBase
+    {
+        JokeModels.FunnyDatabaseContext context = new JokeModels.FunnyDatabaseContext();
+
+        //GET: api/jokes
+       [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(context.Jokes.ToList());
+        }
+
+        // DELETE api/jokes/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var törlendőVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            context.Remove(törlendőVicc);
+            context.SaveChanges();
+        }
+    }
+}
